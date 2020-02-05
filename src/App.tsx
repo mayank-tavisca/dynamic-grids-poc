8aef441058a3a96f1f7059649ab53d4415bd9cf1
Mayank Jain <majain@tavisca.com>
2020-02-05 14:10:28 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import DynamicGrid from "@bit/mayank-tavisca.dynamicgrid.dynamic-grid";
+const Data1 = {
+  headers: [
+    { label: "User Names", value: "username" },
+    { label: "Emails", value: "email" },
+    { label: "Phone number", value: "number" },
+    { label: "Gender", value: "gender" }
+  ],
+  tableData: [
+    {
+      username: "U1",
+      email: "4test1@test.com",
+      number: 6123445566,
+      gender: "M"
+    },
+    {
+      username: "U2",
+      email: "3test2@test.com",
+      number: 3123445566,
+      gender: "F"
+    },
+    {
+      username: "U3",
+      email: "1test3@test.com",
+      number: 2123445566,
+      gender: "M"
+    },
+    {
+      username: "U4",
+      email: "2test4@test.com",
+      number: 9123445566,
+      gender: "F"
+    },
+    {
+      username: "U5",
+      email: "3test2@test.com",
+      number: 3123445566,
+      gender: "F"
+    },
+    {
+      username: "U6",
+      email: "1test3@test.com",
+      number: 2123445566,
+      gender: "M"
+    },
+    {
+      username: "U7",
+      email: "2test4@test.com",
+      number: 9123445566,
+      gender: "F"
+    }
+  ],
+  styles: {},
+  paginationEnabled: true,
+  pagination: {},
+  sortingEnabled: true,
+  sorting: {
+    defaultOrder: "ascending",
+    sortBy: "username"
+  }
+};
 const App = () => {
   return (
     <div className="App">
+      <div className="table1">
+        <DynamicGrid data={Data1} />
+      </div>
+
+      <div className="table2">
+        <DynamicGrid data={Data1} />
+      </div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,6 +89,6 @@ const App = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
